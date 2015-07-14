b87ec80d07d0e214b3a247078093182e33032b87
Bright Antwi Boasiako <brightantwiboasiako@aol.com>
2015-07-14 12:56:01 +0100

    Second commit

---
+++ src/Money/Currency.php
@@ -0,0 +1,133 @@
+<?php namespace Money;
+
+/**
+ * Description of Currency
+ *
+ * @author Bright Antwi Boasiako<brightantwiboasiako@aol.com>
+ */
+class Currency 
+{
+    
+    /**
+     *  The Locale of the currency
+     * 
+     * @var Locale $locale
+     * 
+     * @access private 
+     */
+    private $locale;
+    
+    
+    /**
+     *  The number formatter of the currency
+     * 
+     * @var \NumberFormatter $numberFormatter
+     * 
+     * @access private 
+     */
+    private $numberFormatter;
+    
+    
+    const CURRENCY_DEFAULT_LOCALE = 'ak-GH';
+    
+    
+    /**
+     * public \Money\Currency $locale
+     * 
+     * Constructs a new currency
+     * 
+     * @param String $locale
+     */
+    public function __construct($locale = null)
+    {
+        $this->setLocale($locale);
+        $this->setNumberFormatter($this->getLocale());
+    }
+    
+    
+    /**
+     * private void setNumberFormatter(String $locale)
+     * 
+     * Sets the number formatter of the currency
+     * 
+     * @param String $locale
+     * 
+     * @return void
+     */
+    private function setNumberFormatter($locale)
+    {
+        $this->numberFormatter = new \NumberFormatter($locale, 
+        \NumberFormatter::CURRENCY);
+    }
+    
+    
+    /**
+     * private void setLocale(String $locale)
+     * 
+     * Sets the locale of the currency
+     * 
+     * @param String $locale
+     * 
+     * @return void
+     */
+    private function setLocale($locale)
+    {
+        if($locale != null)
+        {
+            \Locale::setDefault($locale);
+        }
+        else 
+        {
+           \Locale::setDefault(self::CURRENCY_DEFAULT_LOCALE); 
+        }
+        
+        $this->locale = \Locale::getDefault();
+    }
+    
+    
+    /**
+     * public String getLocale(void)
+     * 
+     * Gets the locale of the currency
+     * 
+     * @return String
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+    
+    
+    /**
+     * private \NumberFormatter getNumberFormatter(void)
+     * 
+     * Gets the number formatter for this currency 
+     * instance
+     * 
+     * @return \NumberFormatter
+     */
+    private function getNumberFormatter()
+    {
+        return $this->numberFormatter;
+    }
+    
+    /**
+     * public String getSymbol()
+     * 
+     * Gets the symbol of the currency
+     * 
+     * @return String
+     */
+    public function getSymbol()
+    {
+        return $this->getNumberFormatter()
+                ->getSymbol(\NumberFormatter::CURRENCY_SYMBOL);
+    }
+    
+    
+    public function format($amount)
+    {
+        return $this->getNumberFormatter()->format($amount);
+    }
+    
+}
--- src/Money/Money.php
+++ src/Money/Money.php
@@ -8,4 +8,137 @@
 class Money 
 {
     
+    /**
+     * private double $amount
+     * 
+     * Amount of the money
+     * 
+     * @var double
+     * 
+     * @access private
+     */
+    private $amount = 0.00;
+    
+    
+    /**
+     * private Money\Currency $currency
+     * 
+     * The currency of the money
+     * 
+     * @var Money\Currency
+     * 
+     * @access private
+     */
+    private $currency;
+    
+    
+    /**
+     * public Money\Money(\Money\Currency $currency)
+     * 
+     * Instantiates the currency object
+     * 
+     * @param \Money\Currency $currency
+     */
+    public function __construct($amount, Currency $currency)
+    {
+        $this->setAmount($amount);
+        $this->setCurrency($currency);
+    }
+    
+    
+    /**
+     * private void setAmount(double $amount)
+     * 
+     * Sets the amount of the money
+     * 
+     * @param double $amount
+     * 
+     * @return void
+     */
+    private function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+    
+    
+    /**
+     * public double getAmount(void)
+     * 
+     * Gets the amount of the money
+     * 
+     * 
+     * @return double
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+    
+    
+    /**
+     * public int getWhole(void)
+     * 
+     * Gets the whole number part of the money
+     * 
+     * @return int
+     */
+    public function getWhole()
+    {
+        return floor($this->getAmount());
+    }
+    
+    
+    /**
+     * public int getFraction(void)
+     * 
+     * Gets the fraction part of the money
+     * 
+     * @return int
+     */
+    public function getFraction()
+    {
+        return number_format($this->getAmount() - $this->getWhole(), 2) * 100;
+    }
+    
+    
+    /**
+     * private void setCurrency(\Money\Currency $currency)
+     * 
+     * Sets the currency of the money
+     * 
+     * @param \Money\Currency $currency
+     * 
+     * @return void
+     */
+    private function setCurrency(Currency $currency)
+    {
+        $this->currency = $currency;
+    }
+    
+    /**
+     * private \Money\Currency getCurrency(void)
+     * 
+     * Gets the currency of the money
+     * 
+     * 
+     * @return \Money\Currency
+     */
+    private function getCurrency()
+    {
+        return $this->currency;
+    }
+    
+    
+    /**
+     * public String __toString(void)
+     * 
+     * Gets the string representation of the money
+     * 
+     * @return String
+     */
+    public function __toString()
+    {
+        return $this->getCurrency()->format($this->getAmount());
+    }
+    
 }
